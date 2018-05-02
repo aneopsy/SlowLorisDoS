b535492a5f683d570e8b36bf091984a9f8c259e4
Paul THEIS (AneoPsy) <paultevatheis@gmail.com>
2018-05-02 02:42:29 +0200

    Clean TMP file

--- build/lib.linux-x86_64-2.7/slowloris/__init__.py
+++
@@ -1,7 +0,0 @@
-"""A sample project, showcasing __main__.py and entry_points."""
-
-# This is the __init__.py file of my_project, and is the root of the package.
-# Put anything you want here, or even make it empty, but it MUST exist
-# for everything to work.
-
-# http://go.chriswarrick.com/entry_points
